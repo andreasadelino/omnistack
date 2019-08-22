444be972e865fb8bfec94da5ae650a4682056346
Andreas <andreasaw8@gmail.com>
2019-08-22 14:19:03 -0300

    Configurando mongo local

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,8 @@ const cors = require("cors")
 
 const server = express();
 
-mongoose.connect("mongodb+srv://admin:lRcDWEmDfNcxjq0U@cluster0-r27kd.mongodb.net/omnistack8?retryWrites=true&w=majority", {
+// mongoose.connect("mongodb+srv://admin:lRcDWEmDfNcxjq0U@cluster0-r27kd.mongodb.net/omnistack8?retryWrites=true&w=majority", {
+mongoose.connect("mongodb://mongo:mongo@localhost:27017/admin?retryWrites=true&w=majority", {
     useNewUrlParser: true
 });
 
