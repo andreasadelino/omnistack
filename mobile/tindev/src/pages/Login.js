8d254c9f4675ea2ad96a0b53a30537ef5aeca9c6
Andreas <andreas.devacc@gmail.com>
2019-11-07 17:17:27 -0200

    Criando app mobile

---
+++ mobile/tindev/src/pages/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+});
